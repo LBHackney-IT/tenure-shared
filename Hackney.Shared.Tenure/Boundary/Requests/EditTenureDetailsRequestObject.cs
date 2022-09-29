5bbc9a1f48140cfeb499caafb12d79ee95e2dd73
Huss <57395585+anefiox@users.noreply.github.com>
2022-09-29 14:24:28 +0100

    Chnages to edit object

--- Hackney.Shared.Tenure/Boundary/Requests/EditTenureDetailsRequestObject.cs
+++ Hackney.Shared.Tenure/Boundary/Requests/EditTenureDetailsRequestObject.cs
@@ -8,6 +8,14 @@ namespace Hackney.Shared.Tenure.Boundary.Requests
         public DateTime? StartOfTenureDate { get; set; }
         public DateTime? EndOfTenureDate { get; set; }
         public TenureType TenureType { get; set; }
+        public string TenureSource { get; set; }
         public string PaymentReference { get; set; }
+        public Terminated Terminated { get; set; }
+        public string FundingSource { get; set; }
+        public string NumberOfAdultsInProperty { get; set; }
+        public string NumberOfChildrenInProperty { get; set; }
+        public bool? HasOffsiteStorage { get; set; }
+        public Account Account { get; set; }
+        public int? VersionNumber { get; set; }
     }
 }
