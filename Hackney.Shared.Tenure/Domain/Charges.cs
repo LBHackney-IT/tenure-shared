c0c730ec17c357a5ae716fd01d67639bb67873eb
lbhmenks <127133016+lbhmenks@users.noreply.github.com>
2023-06-07 11:28:50 +0100

    Add storage charge, rent adjustment, and rent adjustment reason to the charges object

--- Hackney.Shared.Tenure/Domain/Charges.cs
+++ Hackney.Shared.Tenure/Domain/Charges.cs
@@ -21,5 +21,22 @@ namespace Hackney.Shared.Tenure.Domain
         public float OriginalRentCharge { get; set; }
 
         public float OriginalServiceCharge { get; set; }
+
+        /// <summary>
+        /// A separately-recorded storage charge
+        /// eg. for tenants using the council's storage facility.
+        /// </summary>
+        public float? StorageCharge { get; set; }
+
+        /// <summary>
+        /// An separately-recorded adjustment made to the standard rent,
+        /// usually a discount (eg. -20f).
+        /// </summary>
+        public float? RentAdjustment { get; set; }
+
+        /// <summary>
+        /// A reason given for the <see cref="RentAdjustment"/>
+        /// </summary>
+        public string RentAdjustmentReason { get; set; }
     }
 }
