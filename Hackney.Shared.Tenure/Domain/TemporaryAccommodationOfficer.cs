cd9e9e9792f41d9de8c0da406373e5491d6d61b5
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-04-11 15:56:16 +0100

    Added Temporary Accommodation details Domain models.
    Co-Authored-By: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

---
+++ Hackney.Shared.Tenure/Domain/TemporaryAccommodationOfficer.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hackney.Shared.Tenure.Domain
+{
+    public class TemporaryAccommodationOfficer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Hackney.Shared.Tenure/Domain/TenureInformation.cs
+++ Hackney.Shared.Tenure/Domain/TenureInformation.cs
@@ -35,6 +35,7 @@ namespace Hackney.Shared.Tenure.Domain
         public int NumberOfAdultsInProperty { get; set; }
         public int NumberOfChildrenInProperty { get; set; }
         public bool? HasOffsiteStorage { get; set; }
+        public TemporaryAccommodationInfo TempAccInfo { get; set; }
         public FurtherAccountInformation FurtherAccountInformation { get; set; }
 
     }
