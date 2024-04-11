36c275db5063959b9eab977f5754775d4f7b6004
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-04-11 16:54:37 +0100

    Added the Domain <--> Entity layer mapping for Temporary Accommodation details.
    Co-Authored-By: Miles Alford <43789512+Miles-Alford@users.noreply.github.com>

--- Hackney.Shared.Tenure.Tests/Factories/EntityFactoryTest.cs
+++ Hackney.Shared.Tenure.Tests/Factories/EntityFactoryTest.cs
@@ -12,17 +12,22 @@ namespace Hackney.Shared.Tenure.Tests.Factories
     {
         private readonly Fixture _fixture = new Fixture();
 
+        #region Tenure Information
         [Fact]
         public void CanMapADatabaseEntityToADomainObject()
         {
             var databaseEntity = _fixture.Create<TenureInformationDb>();
             databaseEntity.EndOfTenureDate = DateTime.UtcNow;
 
-            var entity = databaseEntity.ToDomain();
+            var domainTenure = databaseEntity.ToDomain();
 
-            databaseEntity.Should().BeEquivalentTo(entity, config => config.Excluding(x => x.IsActive));
+            databaseEntity.Should().BeEquivalentTo(domainTenure, config => config.Excluding(x => x.IsActive));
+            // TODO: cover isActive????
+            // testing only the expired tenure???
+
+            // If it is null, cross-static-method calls were not properly covered.
+            domainTenure.TempAccInfo.Should().NotBeNull();
         }
-
 
         [Fact]
         public void CanMapADomainEntityToADatabaseObject()
@@ -33,6 +38,122 @@ namespace Hackney.Shared.Tenure.Tests.Factories
             var databaseEntity = entity.ToDatabase();
 
             entity.Should().BeEquivalentTo(databaseEntity);
+
+            // If it is null, cross-static-method calls were not properly covered.
+            databaseEntity.TempAccInfo.Should().NotBeNull();
         }
+        #endregion
+        #region Temporary Accommodation Information
+        #region Domain to Entity
+        [Fact]
+        public void NullTAOfficerDomainMapsToNullTAOfficerDatabase()
+        {
+            // arrange
+            TemporaryAccommodationOfficer domainTAOfficer = null;
+
+            // act
+            var taOfficerEntity = domainTAOfficer.ToDatabase();
+
+            // assert
+            taOfficerEntity.Should().BeNull();
+        }
+        [Fact]
+        public void NullTAInfoDomainMapsToNullTAInfoDatabase()
+        {
+            // arrange
+            TemporaryAccommodationInfo domainTAInfo = null;
+
+            // act
+            var taInfoEntity = domainTAInfo.ToDatabase();
+
+            // assert
+            taInfoEntity.Should().BeNull();
+        }
+        [Fact]
+        public void TAInfoDomainMapsFieldsCorrectlyToTAInfoDatabase()
+        {
+            // arrange
+            var domainTAInfo = _fixture.Create<TemporaryAccommodationInfo>();
+
+            // act
+            var taInfoEntity = domainTAInfo.ToDatabase();
+
+            // assert
+            taInfoEntity.BookingStatus.Should().Be(domainTAInfo.BookingStatus);
+            // If it is null, cross-static-method calls were not properly covered.
+            taInfoEntity.AssignedOfficer.Should().NotBeNull();
+            taInfoEntity.AssignedOfficer.Should().Be(domainTAInfo.AssignedOfficer);
+        }
+        [Fact]
+        public void TAOfficerDomainMapsFieldsCorrectlyToTAOfficerDatabase()
+        {
+            // arrange
+            var domainTAOfficer = _fixture.Create<TemporaryAccommodationOfficer>();
+
+            // act
+            var taOfficerEntity = domainTAOfficer.ToDatabase();
+
+            // assert
+            taOfficerEntity.FirstName.Should().Be(domainTAOfficer.FirstName);
+            taOfficerEntity.LastName.Should().Be(domainTAOfficer.LastName);
+            taOfficerEntity.Email.Should().Be(domainTAOfficer.Email);
+        }
+        #endregion
+        #region Entity to Domain
+        [Fact]
+        public void NullTAOfficerEntityMapsToNullTAOfficerDomain()
+        {
+            // arrange
+            TemporaryAccommodationOfficerDb entityTAOfficer = null;
+
+            // act
+            var taOfficerDomain = entityTAOfficer.ToDomain();
+
+            // assert
+            taOfficerDomain.Should().BeNull();
+        }
+        [Fact]
+        public void NullTAInfoEntityMapsToNullTAInfoDomain()
+        {
+            // arrange
+            TemporaryAccommodationInfoDb entityTAInfo = null;
+
+            // act
+            var taInfoDomain = entityTAInfo.ToDomain();
+
+            // assert
+            taInfoDomain.Should().BeNull();
+        }
+        [Fact]
+        public void TAInfoEntityMapsFieldsCorrectlyToTAInfoDomain()
+        {
+            // arrange
+            var entityTAInfo = _fixture.Create<TemporaryAccommodationInfoDb>();
+
+            // act
+            var taInfoDomain = entityTAInfo.ToDomain();
+
+            // assert
+            taInfoDomain.BookingStatus.Should().Be(entityTAInfo.BookingStatus);
+            // If it is null, cross-static-method calls were not properly covered.
+            taInfoDomain.AssignedOfficer.Should().NotBeNull();
+            taInfoDomain.AssignedOfficer.Should().Be(entityTAInfo.AssignedOfficer);
+        }
+        [Fact]
+        public void TAOfficerEntityMapsFieldsCorrectlyToTAOfficerDomain()
+        {
+            // arrange
+            var entityTAOfficer = _fixture.Create<TemporaryAccommodationOfficerDb>();
+
+            // act
+            var taOfficerDomain = entityTAOfficer.ToDomain();
+
+            // assert
+            taOfficerDomain.FirstName.Should().Be(entityTAOfficer.FirstName);
+            taOfficerDomain.LastName.Should().Be(entityTAOfficer.LastName);
+            taOfficerDomain.Email.Should().Be(entityTAOfficer.Email);
+        }
+        #endregion
+        #endregion
     }
 }
--- Hackney.Shared.Tenure/Factories/EntityFactory.cs
+++ Hackney.Shared.Tenure/Factories/EntityFactory.cs
@@ -1,3 +1,4 @@
+using Hackney.Shared.Tenure.Boundary.Response;
 using Hackney.Shared.Tenure.Domain;
 using Hackney.Shared.Tenure.Infrastructure;
 
@@ -5,6 +6,8 @@ namespace Hackney.Shared.Tenure.Factories
 {
     public static class EntityFactory
     {
+        #region Tenure Information
+        // Entity to Domain
         public static TenureInformation ToDomain(this TenureInformationDb databaseEntity)
         {
             return new TenureInformation
@@ -39,38 +42,90 @@ namespace Hackney.Shared.Tenure.Factories
             };
         }
 
-        public static TenureInformationDb ToDatabase(this TenureInformation entity)
+        // Domain to Entity
+        public static TenureInformationDb ToDatabase(this TenureInformation domain)
         {
             return new TenureInformationDb
             {
-                Id = entity.Id,
-                Terminated = entity.Terminated,
-                TenureType = entity.TenureType,
-                TenureSource = entity.TenureSource,
-                TenuredAsset = entity.TenuredAsset,
-                SuccessionDate = entity.SuccessionDate,
-                AgreementType = entity.AgreementType,
-                Charges = entity.Charges,
-                EndOfTenureDate = entity.EndOfTenureDate,
-                EvictionDate = entity.EvictionDate,
-                HouseholdMembers = entity.HouseholdMembers.ToListOrEmpty(),
-                InformHousingBenefitsForChanges = entity.InformHousingBenefitsForChanges,
-                IsMutualExchange = entity.IsMutualExchange,
-                IsSublet = entity.IsSublet,
-                IsTenanted = entity.IsTenanted,
-                LegacyReferences = entity.LegacyReferences.ToListOrEmpty(),
-                Notices = entity.Notices.ToListOrEmpty(),
-                PaymentReference = entity.PaymentReference,
-                PotentialEndDate = entity.PotentialEndDate,
-                StartOfTenureDate = entity.StartOfTenureDate,
-                SubletEndDate = entity.SubletEndDate,
-                VersionNumber = entity.VersionNumber,
-                FundingSource = entity.FundingSource,
-                NumberOfAdultsInProperty = entity.NumberOfAdultsInProperty,
-                NumberOfChildrenInProperty = entity.NumberOfChildrenInProperty,
-                HasOffsiteStorage = entity.HasOffsiteStorage,
-                FurtherAccountInformation = entity.FurtherAccountInformation
+                Id = domain.Id,
+                Terminated = domain.Terminated,
+                TenureType = domain.TenureType,
+                TenureSource = domain.TenureSource,
+                TenuredAsset = domain.TenuredAsset,
+                SuccessionDate = domain.SuccessionDate,
+                AgreementType = domain.AgreementType,
+                Charges = domain.Charges,
+                EndOfTenureDate = domain.EndOfTenureDate,
+                EvictionDate = domain.EvictionDate,
+                HouseholdMembers = domain.HouseholdMembers.ToListOrEmpty(),
+                InformHousingBenefitsForChanges = domain.InformHousingBenefitsForChanges,
+                IsMutualExchange = domain.IsMutualExchange,
+                IsSublet = domain.IsSublet,
+                IsTenanted = domain.IsTenanted,
+                LegacyReferences = domain.LegacyReferences.ToListOrEmpty(),
+                Notices = domain.Notices.ToListOrEmpty(),
+                PaymentReference = domain.PaymentReference,
+                PotentialEndDate = domain.PotentialEndDate,
+                StartOfTenureDate = domain.StartOfTenureDate,
+                SubletEndDate = domain.SubletEndDate,
+                VersionNumber = domain.VersionNumber,
+                FundingSource = domain.FundingSource,
+                NumberOfAdultsInProperty = domain.NumberOfAdultsInProperty,
+                NumberOfChildrenInProperty = domain.NumberOfChildrenInProperty,
+                HasOffsiteStorage = domain.HasOffsiteStorage,
+                TempAccInfo = domain.TempAccInfo.ToDatabase(),
+                FurtherAccountInformation = domain.FurtherAccountInformation
             };
         }
+        #endregion
+        #region Temporary Accommodation Information
+        // Entity to Domain
+        public static TemporaryAccommodationOfficer ToDomain(this TemporaryAccommodationOfficerDb taOfficerEntity)
+        {
+            if (taOfficerEntity == null) return null;
+
+            return new TemporaryAccommodationOfficer
+            {
+                FirstName = taOfficerEntity.FirstName,
+                LastName = taOfficerEntity.LastName,
+                Email = taOfficerEntity.Email
+            };
+        }
+        // Entity to Domain
+        public static TemporaryAccommodationInfo ToDomain(this TemporaryAccommodationInfoDb taInfoEntity)
+        {
+            if (taInfoEntity == null) return null;
+
+            return new TemporaryAccommodationInfo
+            {
+                BookingStatus = taInfoEntity.BookingStatus,
+                AssignedOfficer = taInfoEntity.AssignedOfficer.ToDomain()
+            };
+        }
+
+        // Entity to Domain
+        public static TemporaryAccommodationOfficerDb ToDatabase(this TemporaryAccommodationOfficer taOfficerDomain)
+        {
+            if (taOfficerDomain == null) return null;
+
+            return new TemporaryAccommodationOfficerDb
+            {
+                FirstName = taOfficerDomain.FirstName,
+                LastName = taOfficerDomain.LastName,
+                Email = taOfficerDomain.Email
+            };
+        }
+        // Entity to Domain
+        public static TemporaryAccommodationInfoDb ToDatabase(this TemporaryAccommodationInfo taInfoDomain)
+        {
+            if (taInfoDomain == null) return null;
+
+            return new TemporaryAccommodationInfoDb
+            {
+                BookingStatus = taInfoDomain.BookingStatus,
+                AssignedOfficer = taInfoDomain.AssignedOfficer.ToDatabase()
+            };
+        }
+        #endregion
     }
 }
