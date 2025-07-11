d4de264a1ce125eaf035cefdd34d26c502a7b4b8
Sam Drumm <sam.drumm@hackney.gov.uk>
2025-07-11 16:29:21 +0100

    Adds emergency values

---
+++ Hackney.Shared.Tenure/Domain/EmergencyBooking.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Hackney.Shared.Tenure.Domain
+{
+    public class EmergencyBooking
+    {
+        public bool? DraftBooking { get; set; }
+        public bool? IsRentAccountRequired { get; set; }
+        public string NoRentAccountReason { get; set; }
+        public DateTime? RentLetterSentDate { get; set; }
+        public DateTime? RentCardGivenDate { get; set; }
+        public DateTime? TenureAcceptedDate { get; set; }
+        public bool? IsSection208NoticeSent { get; set; }
+    }
+}
--- Hackney.Shared.Tenure/Domain/TemporaryAccommodationInfo.cs
+++ Hackney.Shared.Tenure/Domain/TemporaryAccommodationInfo.cs
@@ -6,5 +6,6 @@ namespace Hackney.Shared.Tenure.Domain
     {
         public string BookingStatus { get; set; }
         public TemporaryAccommodationOfficer AssignedOfficer { get; set; }
+        public EmergencyBooking EmergencyBooking { get; set; }
     }
 }
